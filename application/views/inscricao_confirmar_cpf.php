6139f12fc415c001dd1b02607e30454583d9d6f2
Kristopher Murata <kris@kside.net>
2010-05-27 16:03:38 -0400

    Adiciona texto com apenas numeros em capos de cpf.

--- application/views/inscricao_confirmar_cpf.php
+++ application/views/inscricao_confirmar_cpf.php
@@ -1,6 +1,6 @@
 <?php echo form_open("inscricao/$tipo"); ?>
 
-    <p><b>CPF:</b> <input type="text" name="cpf" size="12" /></p>
+    <p><b>CPF:</b> <input type="text" name="cpf" size="12" /><font color="blue"> <b>Apenas N&uacute;meros.</b></font></p>
     
     <p align="center"><input type="submit" value="Enviar" class="botao"></p>
 <?php echo form_close(); ?>
